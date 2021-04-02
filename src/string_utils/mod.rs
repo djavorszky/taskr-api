b2d8102c0a08e2a4a310f8728e714e90815d0d30
Daniel <daniel.javorszky@gmail.com>
2021-04-02 21:01:39 +0200

    Persist the type of whitespace

--- src/string_utils/mod.rs
+++ src/string_utils/mod.rs
@@ -1,15 +1,22 @@
 pub fn capitalize<T: AsRef<str>>(val: T) -> String {
+    let mut previous_whitespace = true;
+
     val.as_ref()
-        .split_whitespace()
-        .map(|word| {
-            let mut chars = word.chars();
-            match chars.next() {
-                None => String::new(),
-                Some(c) => c.to_uppercase().collect::<String>() + chars.as_str(),
+        .chars()
+        .map(|c| {
+            if c.is_whitespace() {
+                previous_whitespace = true;
+
+                c.to_string()
+            } else if previous_whitespace == true {
+                previous_whitespace = false;
+
+                c.to_uppercase().collect::<String>()
+            } else {
+                c.to_string()
             }
         })
-        .collect::<Vec<String>>()
-        .join(" ")
+        .collect()
 }
 
 #[cfg(test)]
@@ -39,5 +46,58 @@ mod tests {
     #[test]
     fn test_capitalize_scharf_ss() {
         assert_eq!("SS".to_string(), capitalize("ß"))
+    }
+
+    #[test]
+    fn test_capitalize_sentence() {
+        assert_eq!(
+            "What A Wonderful World".to_string(),
+            capitalize("what a wonderful world")
+        )
+    }
+
+    #[test]
+    fn test_capitalize_sentence_with_new_lines() {
+        assert_eq!(
+            "What\nA\nWonderful\nWorld\n".to_string(),
+            capitalize("what\na\nwonderful\nworld\n")
+        );
+
+        assert_eq!(
+            "What\r\nA\r\nWonderful\r\nWorld\r\n".to_string(),
+            capitalize("what\r\na\r\nwonderful\r\nworld\r\n")
+        );
+    }
+
+    #[test]
+    fn test_capitalize_sentence_with_tabs() {
+        assert_eq!(
+            "What\tA\tWonderful\tWorld".to_string(),
+            capitalize("what\ta\twonderful\tworld")
+        );
+    }
+
+    #[test]
+    fn test_capitalize_sentence_with_mixed_whitespaces() {
+        assert_eq!(
+            "What\tA\tWonderful\nWorld\n".to_string(),
+            capitalize("what\ta\twonderful\nworld\n")
+        );
+    }
+
+    #[test]
+    fn test_capitalize_sentence_with_multiple_consecutive_whitespaces() {
+        assert_eq!(
+            "What\t\tA\t\tWonderful  World\n".to_string(),
+            capitalize("what\t\ta\t\twonderful  world\n")
+        );
+    }
+
+    #[test]
+    fn test_capitalize_sentence_with_mixed_consecutive_whitespaces() {
+        assert_eq!(
+            "What\n\tA\t\nWonderful \r\n World \n   \t  ".to_string(),
+            capitalize("what\n\ta\t\nwonderful \r\n world \n   \t  ")
+        );
     }
 }
